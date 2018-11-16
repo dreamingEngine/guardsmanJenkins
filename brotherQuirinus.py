4fe9b0bf49e53d42f595195fde7345450ec2a6b5
Kunal Malhotra <kunal_malhotra@comcast.com>
2018-11-15 18:26:43 -0800

    updating typo

--- brotherQuirinus.py
+++ brotherQuirinus.py
@@ -19,7 +19,7 @@ class GuardsmanClient(Client):
 		phrasebook = {}
 		phrases = open(filename, "r")
 		for line in phrases:
-			str1, str2 = line.split(",")
+			str1, str2 = line.split("/")
 			phrasebook[str1] = str2
 		print(str(phrasebook))
 		phrases.close()
